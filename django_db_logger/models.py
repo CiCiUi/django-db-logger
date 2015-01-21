194e8899f15c3f0a209cacb473d271e1830275f3
zhangshine <zhangshine0125@gmail.com>
2015-01-22 00:04:51 +0800

    model

--- django_db_logger/models.py
+++ django_db_logger/models.py
@@ -1,3 +1,26 @@
+import logging
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+LOG_LEVELS = (
+    (logging.NOTSET, _('NotSet')),
+    (logging.INFO, _('Info')),
+    (logging.WARNING, _('Warning')),
+    (logging.DEBUG, _('Debug')),
+    (logging.ERROR, _('Error')),
+    (logging.FATAL, _('Fatal')),
+)
+
+
+class StatusLog(models.Model):
+    logger_name = models.CharField(max_length=100)
+    level = models.PositiveSmallIntegerField(choices=LOG_LEVELS, default=logging.ERROR, db_index=True)
+    msg = models.TextField()
+    trace = models.TextField(blank=True, null=True)
+    create_datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.msg
+
+    class Meta:
+        ordering = ('-create_datetime',)

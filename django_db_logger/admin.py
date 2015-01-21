e2a2f8e8003b1671c1c5f43ccb5c0b1b31b290a7
zhangshine <zhangshine0125@gmail.com>
2015-01-22 00:39:20 +0800

    table header short description

--- django_db_logger/admin.py
+++ django_db_logger/admin.py
@@ -7,13 +7,12 @@ from .models import StatusLog
 
 
 class StatusLogAdmin(admin.ModelAdmin):
-    list_display = ('colored_msg', 'traceback', 'create_datetime')
+    list_display = ('colored_msg', 'traceback', 'create_datetime_format')
     list_display_links = ('colored_msg', )
     list_filter = ('level', )
     list_per_page = 10
 
-    @staticmethod
-    def colored_msg(instance):
+    def colored_msg(self, instance):
         if instance.level in [logging.NOTSET, logging.INFO, logging]:
             color = 'green'
         elif instance.level in [logging.WARNING, logging.DEBUG]:
@@ -21,10 +20,14 @@ class StatusLogAdmin(admin.ModelAdmin):
         else:
             color = 'red'
         return format_html('<span style="color: {color};">{msg}</span>', color=color, msg=instance.msg)
+    colored_msg.short_description = 'Message'
 
-    @staticmethod
-    def traceback(instance):
+    def traceback(self, instance):
         return format_html('<pre><code>{content}</code></pre>', content=instance.trace)
+
+    def create_datetime_format(self, instance):
+        return instance.create_datetime.strftime('%Y-%m-%d %X')
+    create_datetime_format.short_description = 'Create Datetime'
 
 
 admin.site.register(StatusLog, StatusLogAdmin)

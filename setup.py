c71e7bfdc7a3bcd516d502e02d2fc0a507ea14a0
zhangshine <zhangshine0125@gmail.com>
2015-01-22 23:31:15 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-db-logger',
+    version='0.1',
+    packages=['django_db_logger'],
+    include_package_data=True,
+    license='MIT License',
+    description='Save logging message to database',
+    long_description=README,
+    url='https://ciciui.com/',
+    author='zhangshine',
+    author_email='zhangshine0125@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

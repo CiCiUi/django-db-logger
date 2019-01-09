eb91f6e5c48408a339582d246e4a53bd1fc69c0f
zhangshine <zhangshine0125@gmail.com>
2019-01-09 08:50:42 +0800

    rename app and model

---
+++ django_db_logger/__init__.py
@@ -0,0 +1,1 @@
+default_app_config='django_db_logger.apps.DbLoggerAppConfig'
---
+++ django_db_logger/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DbLoggerAppConfig(AppConfig):
+    name = 'django_db_logger'
+    verbose_name = 'Db logging'

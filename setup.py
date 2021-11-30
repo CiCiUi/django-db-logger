a56f8c67a3320a0321e739e827f444ca9bd5f50b
zhangshine <zhangshine0125@gmail.com>
2021-11-30 21:31:59 +0800

    v0.1.11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-db-logger',
-    version='0.1.10',
+    version='0.1.11',
     packages=['django_db_logger', 'django_db_logger.migrations'],
     include_package_data=True,
     license='MIT License',
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/CiCiUi/django-db-logger',
     author='zhangshine',
     author_email='zhangshine0125@gmail.com',
-    install_requires=['django>=1.6', 'six'],
+    install_requires=['django>=1.9', 'six'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -28,8 +28,10 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],

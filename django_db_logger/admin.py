f09d1f3384fdde6b7de0c5ed1abe9eac40be6519
Sezer BOZKIR <admin@sezerbozkir.com>
2023-11-28 18:33:03 +0300

    admin panel 'created at' time is made to aware of the timezone

--- django_db_logger/admin.py
+++ django_db_logger/admin.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import logging
 
 from django.contrib import admin
+from django.utils import timezone
 from django.utils.html import format_html
 
 from django_db_logger.config import DJANGO_DB_LOGGER_ADMIN_LIST_PER_PAGE
@@ -10,8 +11,8 @@ from .models import StatusLog
 
 class StatusLogAdmin(admin.ModelAdmin):
     list_display = ('colored_msg', 'traceback', 'create_datetime_format')
-    list_display_links = ('colored_msg', )
-    list_filter = ('level', )
+    list_display_links = ('colored_msg',)
+    list_filter = ('level',)
     list_per_page = DJANGO_DB_LOGGER_ADMIN_LIST_PER_PAGE
 
     def colored_msg(self, instance):
@@ -22,13 +23,15 @@ class StatusLogAdmin(admin.ModelAdmin):
         else:
             color = 'red'
         return format_html('<span style="color: {color};">{msg}</span>', color=color, msg=instance.msg)
+
     colored_msg.short_description = 'Message'
 
     def traceback(self, instance):
         return format_html('<pre><code>{content}</code></pre>', content=instance.trace if instance.trace else '')
 
     def create_datetime_format(self, instance):
-        return instance.create_datetime.strftime('%Y-%m-%d %X')
+        return timezone.localtime(instance.create_datetime).strftime('%Y-%m-%d %X')
+
     create_datetime_format.short_description = 'Created at'
 
 
